768849876870353f04eb65cbcfb0cbb9e9a100b8
Zababurin <zababurinsv@vk.com>
2024-08-22 03:32:20 +0300

    update

---
+++ bootstrappers.js
@@ -0,0 +1,6 @@
+// Find this list at: https://github.com/ipfs/js-ipfs/blob/master/packages/ipfs-core-config/src/config.js
+export default [
+  '/dns4/discovery-biq5.onrender.com/tcp/443/p2p/12D3KooWR1HGc4mHKx4t9wssX4Tat5FzSbJZ9sduFLSqh31zZc6r',
+  '/dns4/127.0.0.1/tcp/4818/p2p/12D3KooWQNvxURk8YAYRhfyPAw3YvyjUaygqAmVeX6HW58ZCvTFN',
+  '/dns4/127.0.0.1/tcp/4839/p2p/12D3KooWJ46sS3Pi84KZpGwCqk7nsUWWAvnftEQsy6xXec2ZitT1',
+]
